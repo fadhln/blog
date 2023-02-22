a97ef1e6b865c5f927a35db54a00f619cc5cc816
Muhammad Fadhlan <muhammad.fadhlan@shopee.com>
2023-02-22 14:07:10 +0700

    feat(home): finish tech stack section

--- src/sections/home/TechStackSection.tsx
+++ src/sections/home/TechStackSection.tsx
@@ -1,45 +1,139 @@
-import React from 'react';
+import { technologies, techStackData } from '@/data/techstackData';
+import useHover from '@/hooks/useHover';
+import cx from '@/utils/cx';
 
-const StackChip: React.FC<{ content: string }> = ({ content }) => {
+import React, { useEffect, useState } from 'react';
+
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const StackChip: React.FC<{
+  content: string;
+  setHoverChip: (s: string | undefined) => void;
+}> = ({ content, setHoverChip }) => {
+  const [chipRef, isChipHover] = useHover();
+  useEffect(() => {
+    if (isChipHover) {
+      setHoverChip(content);
+    } else {
+      setHoverChip(undefined);
+    }
+  }, [isChipHover, content, setHoverChip]);
+
   return (
-    <div className="bg-warm-gray-200 whitespace-nowrap dark:bg-warm-gray-700 border border-warm-gray-300 dark:border-warm-gray-600 py-0.5 px-2 text-xs font-normal dark:font-light rounded-full">
+    <div
+      ref={chipRef}
+      className="bg-warm-gray-200 whitespace-nowrap cursor-pointer hover:bg-warm-gray-100 hover:dark:bg-warm-gray-600 dark:bg-warm-gray-700 border border-warm-gray-300 dark:border-warm-gray-600 py-0.5 px-2 text-xs font-normal dark:font-light rounded-full transition-all"
+    >
       {content}
     </div>
   );
 };
 
 const TechStackSection = () => {
+  const [hoveredChip, setHoveredChip] = useState<string>();
+
   return (
-    <section id="tech-stack" className="mx-auto container">
+    <section id="tech-stack" className="mx-auto container px-3">
       <div className="mb-2 flex justify-end w-full font-light text-sm md:text-base text-center">
         <p>
           <span className="text-rose-500 font-normal">*)</span> I work with a
           lot of stuff
         </p>
       </div>
-      <div className="bg-warm-gray-100 dark:bg-warm-gray-800 border border-warm-gray-400 dark:border-warm-gray-600 rounded-lg shadow-lg p-8 z-30">
+      <motion.div
+        variants={{
+          offscreen: {
+            y: -10,
+            opacity: 0,
+            rotateX: 20,
+            transition: {
+              type: 'keyframes',
+              duration: 0.8,
+            },
+          },
+          onscreen: {
+            y: 0,
+            opacity: 100,
+            rotateX: 0,
+            transition: {
+              type: 'keyframes',
+              duration: 0.2,
+            },
+          },
+        }}
+        initial={'offscreen'}
+        whileInView={'onscreen'}
+        viewport={{
+          once: false,
+          amount: 0.2,
+        }}
+        className="bg-warm-gray-100 dark:bg-warm-gray-800 border border-warm-gray-400 dark:border-warm-gray-600 rounded-lg shadow-lg p-8 z-30"
+      >
         <h2 className="text-3xl font-normal text-center">
           Tech I&apos;ve tinkered with
         </h2>
         <div className="flex justify-center mt-4">
-          <div className="relative overflow-hidden max-w-sm">
-            <div className="absolute top-0 left-0 bg-gradient-to-r pointer-events-none h-[4rem] z-30 w-[4rem] from-warm-gray-100 to-warm-gray-100/0" />
-            <div className="absolute top-0 right-0 bg-gradient-to-l pointer-events-none h-[4rem] z-30 w-[4rem] from-warm-gray-100 to-warm-gray-100/0" />
-            <div className="flex animate-marquee w-fit gap-2 z-10">
-              <StackChip content="Front-end Development" />
-              <StackChip content="Back-end Development" />
-              <StackChip content="Internet of Things" />
-              <StackChip content="Artificial Intelligence" />
+          <div className="relative overflow-hidden max-w-sm group">
+            <div className="absolute top-0 left-0 bg-gradient-to-r pointer-events-none h-[4rem] z-30 w-[4rem] from-warm-gray-100 dark:from-warm-gray-800 to-warm-gray-100/0 dark:to-warm-gray-800/0" />
+            <div className="absolute top-0 right-0 bg-gradient-to-l pointer-events-none h-[4rem] z-30 w-[4rem] from-warm-gray-100 dark:from-warm-gray-800 to-warm-gray-100/0 dark:to-warm-gray-800/0" />
+            <div className="flex animate-marquee w-fit gap-2 z-10 group-hover:pause">
+              {technologies.map((techs) => (
+                <StackChip
+                  key={techs}
+                  content={techs}
+                  setHoverChip={setHoveredChip}
+                />
+              ))}
             </div>
-            <div className="flex animate-marquee2 absolute top-0 z-10 w-fit gap-2 mx-2">
-              <StackChip content="Front-end Development" />
-              <StackChip content="Back-end Development" />
-              <StackChip content="Internet of Things" />
-              <StackChip content="Artificial Intelligence" />
+            <div className="flex animate-marquee2 absolute top-0 z-10 w-fit gap-2 mx-2 group-hover:pause">
+              {technologies.map((techs) => (
+                <StackChip
+                  key={techs}
+                  content={techs}
+                  setHoverChip={setHoveredChip}
+                />
+              ))}
             </div>
           </div>
         </div>
-      </div>
+        <div className="mt-12 flex justify-around gap-2 mx-auto max-w-6xl flex-wrap">
+          {techStackData.map((data) => {
+            return (
+              <Tooltip.Provider key={data.name} delayDuration={100}>
+                <Tooltip.Root>
+                  <Tooltip.Trigger asChild>
+                    <div className={'relative w-[4rem] h-[3rem]'}>
+                      <Image
+                        src={data.src}
+                        alt={data.name}
+                        fill
+                        className={cx(
+                          'object-contain transition-opacity invert dark:invert-0',
+                          hoveredChip === data.tech
+                            ? 'opacity-100'
+                            : 'opacity-60 hover:opacity-100'
+                        )}
+                      />
+                    </div>
+                  </Tooltip.Trigger>
+                  <Tooltip.Portal>
+                    <Tooltip.Content
+                      className="bg-warm-gray-300 dark:bg-warm-gray-700 text-warm-gray-900 dark:text-warm-gray-50 px-2 py-1 rounded-lg shadow-md text-sm animate-slideUpAndFade"
+                      sideOffset={5}
+                      data-side="bottom"
+                    >
+                      {data.name}
+                      <Tooltip.Arrow className="fill-warm-gray-300 dark:fill-warm-gray-700" />
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                </Tooltip.Root>
+              </Tooltip.Provider>
+            );
+          })}
+        </div>
+      </motion.div>
     </section>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -33,6 +33,13 @@ module.exports = {
       animation: {
         marquee: 'marquee 35s linear infinite',
         marquee2: 'marquee2 35s linear infinite',
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade:
+          'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade:
+          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
       keyframes: {
         marquee: {
@@ -42,6 +49,22 @@ module.exports = {
         marquee2: {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(0%)' },
+        },
+        slideDownAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(-2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideUpAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
         },
       },
     },
